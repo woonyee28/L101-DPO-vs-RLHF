92ac60476e1eaaa3f716cafadfdd4a6a8f77a7fa
woonyee28 <ngnwy289@gmail.com>
2025-11-13 16:20:24 +0000

    dev: dpo training code done

--- src/training/dpo_trainer.py
+++ src/training/dpo_trainer.py
@@ -1,5 +1,5 @@
-from src.data.dataset_loader import DatasetLoader
-from src.models.pythia_model import PythiaModel
+from ..data.dataset_loader import DatasetLoader
+from ..models.pythia_model import PythiaModel
 from trl import DPOTrainer, DPOConfig
 
 import logging
@@ -7,14 +7,18 @@ import logging
 logger = logging.getLogger(__name__)
 
 class DPO_Trainer:
-    def __init__(self, model: PythiaModel.model, args: None, processing_class: PythiaModel.tokenizer, train_dataset: DatasetLoader.train_dataset):
+    def __init__(self, model, processing_class, train_dataset, valid_ds, args = None):
         self.model = model
         self.args = args
         self.processing_class = processing_class
         self.train_dataset = train_dataset
+        self.valid_ds = valid_ds
+
+        if args is None:
+            self.args = DPOConfig(output_dir="./pythia-70m-deduped-DPO")
 
         logger.info("Initializing DPOTrainer...")
-        self.trainer = DPOTrainer(model=model, args=args, processing_class=processing_class, train_dataset=train_dataset)
+        self.trainer = DPOTrainer(model=model, args=self.args, processing_class=processing_class, train_dataset=train_dataset, eval_dataset=valid_ds)
         logger.info("DPOTrainer initialized successfully!")
 
     def train(self):
