990c290264feebbdf83bc4b3e5484a8c405c5e55
woonyee28 <ngnwy289@gmail.com>
2025-12-04 15:14:00 +0000

    dev: starting point for 31m model

---
+++ src/evals/run.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+MODELS=(
+  "woon/pythia-70m-dpo-100-0"
+  "woon/pythia-70m-dpo-80-20"
+  "woon/pythia-70m-dpo-50-50"
+  "woon/pythia-70m-ppo-100-0"
+  "woon/pythia-70m-ppo-80-20"
+  "woon/pythia-70m-ppo-50-50"
+  "woon/pythia-160m-dpo-100-0"
+  "woon/pythia-160m-dpo-80-20"
+  "woon/pythia-160m-dpo-50-50"
+  "woon/pythia-160m-ppo-100-0"
+  "woon/pythia-160m-ppo-80-20"
+  "woon/pythia-160m-ppo-50-50"
+)
+
+
+for MODEL_NAME in "${MODELS[@]}"; do
+  echo "Running evaluation for model: $MODEL_NAME"
+  
+  helm-run --run-entries \
+    truthful_qa:task=mc_single,model=$MODEL_NAME \
+    bbq:subject=all,model=$MODEL_NAME \
+    bbq:subject=Age,model=$MODEL_NAME \
+    bbq:subject=Disability_status,model=$MODEL_NAME \
+    bbq:subject=Gender_identity,model=$MODEL_NAME \
+    bbq:subject=Race_ethnicity,model=$MODEL_NAME \
+    bbq:subject=Religion,model=$MODEL_NAME \
+    bbq:subject=Sexual_orientation,model=$MODEL_NAME \
+    disinformation:capability=reiteration,topic=covid,model=$MODEL_NAME \
+    disinformation:capability=wedging,model=$MODEL_NAME \
+    --suite bias-evaluation --max-eval-instances 1000 --disable-cache \
+    --local-path prod_env
+  
+  echo "Completed evaluation for model: $MODEL_NAME"
+  echo "----------------------------------------"
+done
+
+helm-summarize --suite bias-evaluation
+
+helm-server --port 8001
+
+
---
+++ src/evals/run_single.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+
+MODELS=(
+  "woon/pythia-70m-ppo-100-0"
+)
+
+
+for MODEL_NAME in "${MODELS[@]}"; do
+  echo "Running evaluation for model: $MODEL_NAME"
+  
+  helm-run --run-entries \
+    truthful_qa:task=mc_single,model=$MODEL_NAME \
+    bbq:subject=all,model=$MODEL_NAME \
+    bbq:subject=Age,model=$MODEL_NAME \
+    bbq:subject=Disability_status,model=$MODEL_NAME \
+    bbq:subject=Gender_identity,model=$MODEL_NAME \
+    bbq:subject=Race_ethnicity,model=$MODEL_NAME \
+    bbq:subject=Religion,model=$MODEL_NAME \
+    bbq:subject=Sexual_orientation,model=$MODEL_NAME \
+    disinformation:capability=reiteration,topic=covid,model=$MODEL_NAME \
+    disinformation:capability=wedging,model=$MODEL_NAME \
+    --suite bias-evaluation --max-eval-instances 1000 --disable-cache \
+    --local-path prod_env
+  
+  echo "Completed evaluation for model: $MODEL_NAME"
+  echo "----------------------------------------"
+done
+
+helm-summarize --suite bias-evaluation
+
+helm-server --port 8001
+
+
